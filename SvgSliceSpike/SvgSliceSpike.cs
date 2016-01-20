04f0f90d7364fdeea11fb3db205e5f15cafbbbc4
Adam Patridge <patridge@gmail.com>
2016-01-20 16:07:28 -0700

    Make SVG render with proper aspect ratio on both platforms.

--- SvgSliceSpike/SvgSliceSpike.cs
+++ SvgSliceSpike/SvgSliceSpike.cs
@@ -6,16 +6,19 @@ using SVG.Forms.Plugin.Abstractions;
 namespace SvgSliceSpike {
     public class App : Application {
         public App() {
+            var SvgSliceInsets = new ResizableSvgInsets(5);
             // The root page of your application
             MainPage = new ContentPage {
                 Content = new StackLayout {
                     VerticalOptions = LayoutOptions.Center,
+                    HorizontalOptions = LayoutOptions.Center,
                     Children = {
                         new SvgImage() {
                             SvgPath = "SvgSliceSpike.Assets.twintechs-logo.svg",
                             SvgAssembly = typeof(App).GetTypeInfo().Assembly, 
-                            HeightRequest = 100,
-                            WidthRequest = 100
+                            Svg9SliceInsets = SvgSliceInsets,
+                            WidthRequest = 139,
+                            HeightRequest = 79,
                         },
                     },
                 },
