3be7abb41fee469b2579e839086b9cf64afe8ccd
Adam Patridge <patridge@gmail.com>
2016-01-20 16:06:02 -0700

    Equality comparison for ResizableSvgInsets.

--- lib/Xamarin.Forms.Plugins/SVG/SVG/SVG.Forms.Plugin.Abstractions/SvgImage.cs
+++ lib/Xamarin.Forms.Plugins/SVG/SVG/SVG.Forms.Plugin.Abstractions/SvgImage.cs
@@ -1,23 +1,58 @@
 ﻿using System.Reflection;
 using Xamarin.Forms;
+using System;
 
 namespace SVG.Forms.Plugin.Abstractions
 {
-    public struct ResizableSvgInsets {
-        public double Top { get; set; }
-        public double Right { get; set; }
-        public double Bottom { get; set; }
-        public double Left { get; set; }
-
-        public ResizableSvgInsets(double top, double right, double bottom, double left) {
-            Top = top;
-            Right = right;
-            Bottom = bottom;
-            Left = left;
-        }
-        public ResizableSvgInsets(double vertical, double horizontal) : this(vertical, horizontal, vertical, horizontal) { }
-        public ResizableSvgInsets(double allSides) : this(allSides, allSides, allSides, allSides) { }
+  public struct ResizableSvgInsets : IEquatable<ResizableSvgInsets> {
+    public double Top { get; set; }
+    public double Right { get; set; }
+    public double Bottom { get; set; }
+    public double Left { get; set; }
+    
+    public static ResizableSvgInsets Zero = new ResizableSvgInsets(0, 0, 0, 0);
+    public ResizableSvgInsets(double top, double right, double bottom, double left) {
+      Top = top;
+      Right = right;
+      Bottom = bottom;
+      Left = left;
     }
+    public ResizableSvgInsets(double vertical, double horizontal) : this(vertical, horizontal, vertical, horizontal) { }
+    public ResizableSvgInsets(double allSides) : this(allSides, allSides, allSides, allSides) { }
+
+    public override bool Equals(object obj)
+    {
+      if (obj.GetType() != typeof(ResizableSvgInsets)) {
+        return false;
+      }
+      return Equals((ResizableSvgInsets)obj);
+    }
+
+    public override int GetHashCode()
+    {
+      return (Top + Right + Bottom + Left).GetHashCode();
+    }
+
+    public static bool operator ==(ResizableSvgInsets inset1, ResizableSvgInsets inset2) 
+    {
+      return inset1.Equals(inset2);
+    }
+
+    public static bool operator !=(ResizableSvgInsets inset1, ResizableSvgInsets inset2) 
+    {
+      return !inset1.Equals(inset2);
+    }
+
+    #region IEquatable implementation
+    public bool Equals(ResizableSvgInsets other)
+    {
+      return Math.Abs(Top - other.Top) < double.Epsilon
+        && Math.Abs(Right - other.Right) < double.Epsilon
+        && Math.Abs(Bottom - other.Bottom) < double.Epsilon
+        && Math.Abs(Left - other.Left) < double.Epsilon;
+    }
+    #endregion
+  }
 
   public class SvgImage : Image
   {
