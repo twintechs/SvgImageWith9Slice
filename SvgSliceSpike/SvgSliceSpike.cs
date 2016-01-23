97feef86b3af8f6dc19a0f7c345d2abde7f47f19
Adam Patridge <patridge@gmail.com>
2016-01-23 14:49:35 -0700

    Demo for slice size changing.

--- SvgSliceSpike/SvgSliceSpike.cs
+++ SvgSliceSpike/SvgSliceSpike.cs
@@ -2,24 +2,78 @@
 
 using Xamarin.Forms;
 using SVG.Forms.Plugin.Abstractions;
+using System.ComponentModel;
+using System.Threading.Tasks;
 
 namespace SvgSliceSpike {
+    public class TestModel : INotifyPropertyChanged {
+        double _AllSidesInset;
+        public double AllSidesInset {
+            get { return _AllSidesInset; }
+            set {
+                if (value != _AllSidesInset) {
+                    _AllSidesInset = value;
+                    _SvgInsets = new ResizableSvgInsets(AllSidesInset);
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(AllSidesInset)));
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(SvgInsets)));
+                }
+            }
+        }
+        ResizableSvgInsets _SvgInsets;
+        public ResizableSvgInsets SvgInsets {
+            get {
+                return _SvgInsets;
+            }
+        }
+
+        public TestModel() {
+            AllSidesInset = 38;
+        }
+
+        #region INotifyPropertyChanged implementation
+        public event PropertyChangedEventHandler PropertyChanged = delegate { };
+        #endregion
+    }
     public class App : Application {
+        readonly TestModel _ViewModel;
+        SvgImage _SlicingSvg;
+        readonly Slider _InsetSlider;
         public App() {
-            var SvgSliceInsets = new ResizableSvgInsets(5);
+            _ViewModel = new TestModel();
+            _InsetSlider = new Slider() {
+                Minimum = 0,
+                Maximum = 39.5,
+                Value = _ViewModel.AllSidesInset,
+            };
+            _SlicingSvg = new SvgImage() {
+                SvgPath = "SvgSliceSpike.Assets.twintechs-logo.svg",
+                SvgAssembly = typeof(App).GetTypeInfo().Assembly,
+                SvgStretchableInsets = new ResizableSvgInsets(_InsetSlider.Value),
+                WidthRequest = 300,
+                HeightRequest = 300,
+//                WidthRequest = 139,
+//                HeightRequest = 79,
+            };
+
+            _InsetSlider.ValueChanged += (sender, e) => {
+                // HACK: SvgImage can't be changed after creation (yet). Toss the old and create a new one.
+                _SlicingSvg.SvgStretchableInsets = new ResizableSvgInsets(e.NewValue);
+            };
+
             // The root page of your application
             MainPage = new ContentPage {
-                Content = new StackLayout {
-                    VerticalOptions = LayoutOptions.Center,
-                    HorizontalOptions = LayoutOptions.Center,
-                    Children = {
-                        new SvgImage() {
-                            SvgPath = "SvgSliceSpike.Assets.twintechs-logo.svg",
-                            SvgAssembly = typeof(App).GetTypeInfo().Assembly, 
-                            Svg9SliceInsets = SvgSliceInsets,
-                            WidthRequest = 139,
-                            HeightRequest = 79,
+                Content = new ScrollView {
+                    Content = new StackLayout {
+                        VerticalOptions = LayoutOptions.Center,
+                        HorizontalOptions = LayoutOptions.Center,
+                        Children = {
+                            _InsetSlider,
+                            _SlicingSvg,
+                            new BoxView {
+                                HeightRequest = 700,
+                            },
                         },
+                        BindingContext = _ViewModel,
                     },
                 },
             };
